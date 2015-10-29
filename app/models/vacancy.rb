40f1d6c4028b28bfcdf9ccdf8f98f9f024a5482f
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-29 18:10:39 +0100

    refs #6 Implements Vacancy#create_project_user?, #calculate_accepted_candidatures_amount, #timezone, #from_raw and #to_raw. Changes vacancy validation.

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -13,23 +13,77 @@ class Vacancy < ActiveRecord::Base
   accepts_nested_attributes_for :candidatures, allow_destroy: true, reject_if: ->(t) { t['name'].blank? }
   scope :open, -> { where(state: 'open') }
   
-  
+  validates :resource_type, presence: true
   validates :project_id, presence: true
   validates :offeror_id, presence: true
-  validates :name, presence: true, uniqueness: { scope: :project_id }
-  validates :text, presence: true
-  validates :limit, presence: true
+  validates :name, presence: true
   
-  attr_accessible :project_id, :name, :text, :limit, :candidatures_attributes
-  
-  extend FriendlyId
-  
-  friendly_id :name, use: :slugged
+  attr_accessible :project_id, :name, :text, :limit, :timezone, :from_raw, :to_raw, :resource_type, :candidatures_attributes
   
   before_validation :set_defaults
   
   def candidatures_left
-    limit - candidatures.accepted.count
+    if limit
+      limit - candidatures.accepted.count
+    else
+      '∞'
+    end
+  end
+  
+  def from_raw
+    timezone && from ? from.in_time_zone(timezone).strftime('%Y-%m-%d %H:%M:%S') : ''
+  end
+  
+  def from_raw=(value)
+    if timezone && value
+      datetime = Time.zone.parse(value)
+      args = [
+        datetime.strftime('%Y').to_i, datetime.strftime('%m').to_i, datetime.strftime('%d').to_i,
+        datetime.strftime('%H').to_i, datetime.strftime('%M').to_i, datetime.strftime('%S').to_i
+      ]
+      timezone_was = Time.zone
+      Time.zone = timezone
+      self.from = Time.zone.local *args
+    end
+  end
+  
+  def to_raw
+    timezone && to ? to.in_time_zone(timezone).strftime('%Y-%m-%d %H:%M:%S') : ''
+  end
+  
+  def to_raw=(value)
+    if timezone && value
+      datetime = Time.zone.parse(value)
+      args = [
+        datetime.strftime('%Y').to_i, datetime.strftime('%m').to_i, datetime.strftime('%d').to_i,
+        datetime.strftime('%H').to_i, datetime.strftime('%M').to_i, datetime.strftime('%S').to_i
+      ]
+      timezone_was = Time.zone
+      Time.zone = timezone
+      self.to = Time.zone.local *args
+    end
+  end
+  
+  def ended?
+    if from.present? && Time.now < from
+      false
+    elsif from.blank? && to.present? && Time.now < to
+      false
+    elsif from.blank? && to.present?
+      true
+    elsif from.blank?
+      false
+    else
+      true
+    end
+  end
+  
+  def calculate_accepted_candidatures_amount
+    candidatures.accepted.count
+  end
+  
+  def create_project_user?
+    true
   end
   
   protected
